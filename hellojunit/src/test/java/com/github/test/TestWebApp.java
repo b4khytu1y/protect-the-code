04f6283921baa1906de66a63dce2b111545446f0
B4khytu1y <bakhytulynurzhan@gmail.com>
2024-08-29 00:12:00 +0500

    hellojunit

--- hellojunit/src/test/java/com/github/test/TestWebApp.java
+++ hellojunit/src/test/java/com/github/test/TestWebApp.java
@@ -1,16 +1,22 @@
 package com.github.test;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.Order;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.model.Employee;
+
 
 /*
 	TODO
@@ -27,6 +33,58 @@ import org.springframework.web.context.WebApplicationContext;
 */
 public class TestWebApp extends SpringBootHelloWorldTests {
 
-	// your solution
+    @Autowired
+    private ApplicationContext applicationContext;
+    private MockMvc mockMvc;
 
+    @Before
+    public void init(){
+        WebApplicationContext wac = (WebApplicationContext) applicationContext;
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    @Order(1)
+    public void getEmployeeTest() throws Exception {
+
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId("1");
+        emp.setSalary(3000);
+
+        mockMvc.perform(get("/employee")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectToJson(emp)));
+    }
+
+    private static String objectToJson(Object object) {
+        try {
+            return new ObjectMapper().writeValueAsString(object);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
